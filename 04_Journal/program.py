46fdafbe9fcc6d6538018e67c8ceb48caee07aa8
Arne Lindow <info@arne-lindow.de>
2018-05-10 18:46:09 +0200

    Starting the Journal

--- 04_Journal/program.py
+++ 04_Journal/program.py
@@ -13,24 +13,29 @@ def run_event_loop():
     print("Was möchtest Du machen?")
 
     cmd = None
+    journal_data = []
 
     while cmd != 'x':
         cmd = input("[L]iste der Einträge, [A]nfügen eines Eintrags, [X] Beenden? ").strip().lower()
         if cmd == 'l':
-            list_entries()
+            list_entries(journal_data)
         elif cmd == 'a':
-            add_entry()
+            add_entry(journal_data)
         elif cmd != 'x':
             print("Entschuldigung, das Kommando {} kenne ich nicht".format(cmd))
     print("Auf Wiedersehen")
 
 
-def list_entries():
-    print("Liste der Einträge")
+def list_entries(data):
+    print("Deine Journal Einträge:")
+    entries = reversed(data)
+    for idx, entry in enumerate(entries):
+        print("{}. {}".format(idx + 1,entry))
 
 
-def add_entry():
-    print("Neuen Eintrag anfügen")
+def add_entry(data):
+    text = input("Gib den neuen Eintrag ein. <Enter> beendet Eingabe: ")
+    data.append(text)
 
 
 main()
