699e2069828a231d63ae3a4ad02d5a2faebbeeeb
Arne Lindow <info@arne-lindow.de>
2018-05-19 17:27:28 +0200

    Movies

--- 98_MyMovieAPI/play.py
+++ 98_MyMovieAPI/play.py
@@ -1,13 +1,46 @@
 import requests
 import collections
 
-search = 'capital'
-key="ef26d57b"
+def search(search):
+    api_key="408ba11ecd062d4001fee034de52a73c"
 
-url = 'http://www.omdbapi.com/?apikey={}&t={}'.format(key,search)
+    page = 1
+    while True:
+        url = 'https://api.themoviedb.org/3/search/movie?query={}&page={}&api_key={}'.format(search,page,api_key)
+        resp = requests.get(url)
+        movie_data = show_response(resp.json(), page)
+        print("Seite {}/ {}".format(movie_data.get("page"),movie_data.get("total_pages")))
+        page += 1
+        if page > resp.json().get("total_pages") or page == 1000:
+            break
 
-resp = requests.get(url)
 
-movie_data = resp.json()
+def show_response(movie_data,page):
+    movie_list = movie_data.get("results")
+    MovieResult = collections.namedtuple(
+        "MovieResult",
+        "vote_count,id,video,vote_average,title,popularity,poster_path,original_language,original_title,genre_ids,backdrop_path,adult,overview,release_date"
+    )
+    movies = []
+    for md in movie_list:
+        m = MovieResult(**md)
+        movies.append(m)
+    movies.sort(key=lambda m: m.release_date, reverse=True)
+    for movie in movies:
+        print("{} -- {} -- {} -- {}".format(movie.release_date, movie.title, movie.original_title, movie.overview))
+    print("Filme {}/{}".format((20 * (page - 1) + len(movies)), movie_data.get("total_results")))
+    return movie_data
 
-print(type(movie_data), movie_data)
+
+def main():
+    print_header()
+    search("runner")
+
+def print_header():
+    print("---------------------------------")
+    print("             TNDB APP")
+    print("---------------------------------")
+    print("This product uses the TMDb API but is not endorsed or certified by TMDb.")
+
+if __name__ == '__main__':
+    main()
