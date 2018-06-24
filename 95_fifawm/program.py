cc6cac9c196c9ef138c3e97c739a208de12e5543
Arne Lindow <info@arne-lindow.de>
2018-06-24 12:37:48 +0200

    load nations form csv file

--- 95_fifawm/program.py
+++ 95_fifawm/program.py
@@ -3,7 +3,6 @@ import csv
 
 from sqlite3 import Error
 
-
 def create_connection(db_file):
     """ create a database connection to the SQLite database
         specified by db_file
@@ -18,7 +17,7 @@ def create_connection(db_file):
         print(e)
     return None
 
-def create_table(conn, sql_ddl_statement):
+def create_sql_stmt(conn, sql_ddl_statement):
     """ create a table from the create_table_sql statement
     :param conn: Connection object
     :param sql_ddl_statement: a CREATE TABLE statement
@@ -40,40 +39,96 @@ def load_nations(conn, filename):
 
     c.executemany("INSERT INTO nation (name, iaaf) VALUES (?, ?);", to_db)
     conn.commit()
-    conn.close()
+
+def load_groups(conn, filename):
+    c = conn.cursor()
+    with open(filename, 'r') as fin:
+        # `with` statement available in 2.5+
+        # csv.DictReader uses first line in file for column headings by default
+        dr = csv.DictReader(fin)  # comma is default delimiter
+        to_db = [(i['name'], i['nation1'], i['nation2'], i['nation3'], i['nation4']) for i in dr]
+
+    c.executemany("INSERT INTO gruppe (name, nation1, nation2,nation3,nation4) VALUES (?, ?, ?, ?, ?);", to_db)
+    conn.commit()
+
+
+def show_data(conn, sql_query):
+    c = conn.cursor()
+    try:
+        c = conn.cursor()
+        cursor = c.execute(sql_query)
+        for row in cursor:
+            print("name = {}".format(row[0]))
+            for i in (1,2,3,4):
+                print("nat = ", row[i])
+            print()
+    except Error as e:
+        print(e)
+
 
 
 def main():
     database = "C:\\sqlite\db\pythonsql.db"
     nation = "teilnehmer.csv"
-
-    sql_drop_nation = """ DROP TABLE nation; """
-    sql_drop_spiel = """ DROP TABLE spiel; """
-    sql_init_encoding = """PRAGMA encoding="UTF-8";"""
+    group = "gruppe.csv"
 
     sql_create_nation_table = """ CREATE TABLE IF NOT EXISTS nation (
-                                            id integer PRIMARY KEY,
+                                            iaaf text PRIMARY KEY,
+                                            name text NOT NULL
+                                        ); """
+
+    sql_create_gruppe_table = """ CREATE TABLE IF NOT EXISTS gruppe (
                                             name text NOT NULL,
-                                            iaaf text NOT NUll
+                                            nation1 text NOT NULL,
+                                            nation2 text NOT NULL,
+                                            nation3 text NOT NULL,
+                                            nation4 text NOT NULL,
+                                                 FOREIGN KEY (nation1) REFERENCES nation(isaaf),
+                                                 FOREIGN KEY (nation2) REFERENCES nation(isaaf),
+                                                 FOREIGN KEY (nation3) REFERENCES nation(isaaf),
+                                                 FOREIGN KEY (nation4) REFERENCES nation(isaaf)
                                         ); """
 
     sql_create_spiel_table = """ CREATE TABLE IF NOT EXISTS spiel (
-                                            nation1_id integer NOT NULL,
-                                            nation2_id integer NOT NULL,
-                                                 FOREIGN KEY (nation1_id) REFERENCES nation(id),
-                                                 FOREIGN KEY (nation2_id) REFERENCES nation(id)
+                                            nation1 text NOT NULL,
+                                            nation2 text NOT NULL,
+                                                 FOREIGN KEY (nation1) REFERENCES nation(isaaf),
+                                                 FOREIGN KEY (nation2) REFERENCES nation(isaaf)
                                         ); """
+
+    sql_show_groups = """ select gruppe.name, n1.name,n2.name,n3.name, n4.name 
+                                 from gruppe 
+                                 join nation as n1 on nation1 = n1.iaaf  
+                                 join nation as n2 on nation2 = n2.iaaf 
+                                 join nation as n3 on nation3 = n3.iaaf 
+                                 join nation as n4 on nation4 = n4.iaaf
+                                 ;"""
+
 
     conn = create_connection(database)
     if conn is not None:
-        create_table(conn, sql_drop_spiel)
-        create_table(conn, sql_drop_nation)
-        create_table(conn, sql_init_encoding)
-        create_table(conn, sql_create_nation_table)
-        create_table(conn, sql_create_spiel_table)
+        reset_db(conn)
+        create_sql_stmt(conn, sql_create_nation_table)
+        create_sql_stmt(conn, sql_create_spiel_table)
+        create_sql_stmt(conn, sql_create_gruppe_table)
         load_nations(conn, nation)
+        load_groups(conn, group)
+
+        show_data(conn, sql_show_groups)
+
         conn.close()
 
+
+def reset_db(conn):
+    sql_drop_nation = """ DROP TABLE nation; """
+    sql_drop_spiel = """ DROP TABLE spiel; """
+    sql_drop_gruppe = """ DROP TABLE gruppe; """
+    sql_init_encoding = """PRAGMA encoding="UTF-8";"""
+
+    create_sql_stmt(conn, sql_drop_spiel)
+    create_sql_stmt(conn, sql_drop_nation)
+    create_sql_stmt(conn, sql_drop_gruppe)
+    create_sql_stmt(conn, sql_init_encoding)
 
 
 if __name__ == '__main__':
